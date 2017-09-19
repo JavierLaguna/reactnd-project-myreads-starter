f413a2376e11eb2013f888a76ed1cd1c9dc1ccf7
Javier Laguna <javier.laguna92@gmail.com>
2017-09-19 17:23:47 +0200

    controlled input

--- src/01-containers/SearchBooks/index.js
+++ src/01-containers/SearchBooks/index.js
@@ -9,7 +9,8 @@ import './index.css';
 export default class SearchBooks extends PureComponent {
     state = {
         books: [],
-        isLoading: false
+        isLoading: false,
+        searchValue: ''
     };
 
     static propTypes = {
@@ -22,13 +23,19 @@ export default class SearchBooks extends PureComponent {
     };
 
     onChangeSearchValue = (event) => {
-        this.setState({isLoading: true});
         const {value} = event.target;
+        this.setState({
+            searchValue: value,
+            isLoading: true
+        });
         this.searchBook(value);
     };
 
     searchBook = debounce((value) => {
         BooksAPI.search(value, 20).then((books) => {
+            if (books.error) {
+                books = [];
+            }
             this.setState({
                 books,
                 isLoading: false
@@ -36,23 +43,34 @@ export default class SearchBooks extends PureComponent {
         });
     }, 500);
 
+    updateBook = (bookToUpdate, newShelf) => {
+        this.setState({isLoading: true});
+        BooksAPI.update(bookToUpdate, newShelf).then((data) => {
+            this.setState((prevState) => ({isLoading: false}));
+        });
+    };
+
     render() {
         const {goBack} = this.props;
-        const {books} = this.state;
+        const {books, searchValue} = this.state;
 
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <a className="close-search" onClick={goBack}>Close</a>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" onChange={this.onChangeSearchValue}/>
+                        <input type="text"
+                               placeholder="Search by title or author"
+                               value={searchValue}
+                               onChange={this.onChangeSearchValue}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
                     {this.state.isLoading ?
                         <Loading/>
                         :
-                        <BooksGrid books={books}/>
+                        <BooksGrid books={books} onChangeShelf={this.updateBook}/>
                     }
                 </div>
             </div>
